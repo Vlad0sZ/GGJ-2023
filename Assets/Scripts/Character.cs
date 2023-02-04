a9bf6cd11cc38f626b2ea6a1f85de08f80a25244
Ilya Karleytov <wiftexoff@gmail.com>
2023-02-04 12:40:28 +0500

    Перемещение игрока, прыжок и взаимодействие

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class Character : MonoBehaviour
+{
+    public virtual void Move(Vector2 direction)
+    {
+        
+    }
+
+    private void Interact()
+    {
+        
+    }
+}
