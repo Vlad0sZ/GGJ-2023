a8b928fb89136a8fb1861e5df36cfbc45e03c267
Ilya Karleytov <wiftexoff@gmail.com>
2023-02-05 12:38:51 +0500

    Add location 2

---
+++ Assets/Scripts/Main/TriggerObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Main
+{
+    public class TriggerObject : MonoBehaviour
+    {
+        public Action PlayerTriggered;
+        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if(col.CompareTag("Player"))
+                PlayerTriggered?.Invoke();
+        }
+    }
+}
