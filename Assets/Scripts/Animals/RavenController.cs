6568665720a842653e96283ee432d5a656497d06
Ilya Karleytov <wiftexoff@gmail.com>
2023-02-05 14:11:23 +0500

    add location 4

---
+++ Assets/Scripts/Animals/RavenController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Locations;
+using UnityEngine;
+
+namespace Animals
+{
+    public class RavenController : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            SceneLoader.Instance.LoadScene("Intro");
+        }
+    }
+}
